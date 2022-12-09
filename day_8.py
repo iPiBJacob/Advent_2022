80629e8e3ae7c0edaeb939afaa7498d5f9319126
IPiBJacob <jrapp@wisc.edu>
2022-12-08 23:47:23 -0600

    made scenic scores prettier

--- day_8.py
+++ day_8.py
@@ -39,56 +39,32 @@ def count_edge_visible(grid):
 
     return visible
 
-def scenic_score(grid, source_x, source_y):
+
+def scenic_score_right(grid, source_x, source_y):
     
-    left_score, right_score, top_score, bottom_score = None,None,None,None    
-    source_height = grid[source_y, source_x]
-      
-    for x in range(source_x-1, -1, -1):  # Left Score
-        if grid[source_y, x] >= source_height or x == 0:
-            left_score = source_x - x
-            break
-        
     for x in range(source_x+1, grid.shape[1]):  # Right Score
-        if grid[source_y, x] >= source_height or x == grid.shape[1]-1:
-            right_score = x - source_x
-            break
-        
-    for y in range(source_y-1, -1, -1):  # Top Score
-        if grid[y, source_x] >= source_height or y == 0:
-            top_score = source_y - y
-            break
-        
-    for y in range(source_y+1, grid.shape[0]):
-        if grid[y, source_x] >= source_height or y == grid.shape[0]-1:
-            bottom_score = y - source_y
-            break
-
-    if source_x == 0:
-        left_score=1
-    if source_x == grid.shape[1]-1:
-        right_score=1
-    if source_y == 0:
-        top_score=1
-    if source_y == grid.shape[0]-1:
-        bottom_score=1
-
-    return top_score * bottom_score * left_score * right_score
-
+        if grid[source_y, x] >= grid[source_y, source_x] or x == grid.shape[1]-1:
+            return x - source_x          
+    return 1
+    
+ 
 def assess_scenic(grid):
     
     scenic_scores = grid*0
     
     for y in range(grid.shape[0]):
         for x in range(grid.shape[1]):
-            scenic_scores[y,x] = scenic_score(grid, x, y)
+            right = scenic_score_right(grid, x, y)
+            left = scenic_score_right(np.flip(grid, 1), grid.shape[1]-x-1, y)
+            top = scenic_score_right(np.flip(grid.T, 1), grid.shape[0]-y-1, x)
+            bottom = scenic_score_right(grid.T, y, x)
+            scenic_scores[y,x] = right * left * top * bottom
             
     return scenic_scores
- 
  
 visible = count_edge_visible(grid) 
 scenic_scores = assess_scenic(grid)
 max = np.unravel_index(scenic_scores.argmax(), scenic_scores.shape)
             
 print(f'part 1 : {np.sum(visible)}')  # correct answer 1832
-print(f'part 2 : {scenic_score(grid, max[1], max[0])}')  # correct answer 157320
+print(f'part 2 : {scenic_scores[max]}')  # correct answer 157320
