7544b3b069a01628360e68160873237f906a97f7
iPiBJacob <jrapp@wisc.edu>
2022-12-09 12:58:27 -0600

    day 8 code golf

--- day_8.py
+++ day_8.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 with open('inputs/input_8.txt', 'r') as input_file:
     lines = [line.strip() for line in input_file.readlines()]
@@ -7,41 +6,25 @@ with open('inputs/input_8.txt', 'r') as input_file:
 lines = [[int(char) for char in line] for line in lines]
 grid = np.array(lines)
 
-def count_edge_visible(grid):
-    visible = grid*0
-    
+def count_visible_left_edge(grid, output_matrix):
     for y in range(grid.shape[0]):
         left_max = 0
         for x in range(grid.shape[1]):
-            if x == 0 or grid[y,x] > left_max:
-                left_max = grid[y,x]
-                visible[y, x] = 1
-                
-        right_max = 0
-        for x in range(grid.shape[1]-1, 0, -1):
-            if x == grid.shape[1]-1 or grid[y,x] > right_max:
-                right_max = grid[y,x]
-                visible[y,x] = 1
-                
-    for x in range(grid.shape[1]):
-        top_max = 0
-        for y in range(grid.shape[0]):
-            if y == 0 or grid[y,x] > top_max:
-                top_max = grid[y,x]
-                visible[y,x] = 1
-                
-        bottom_max = 0
-        for y in range(grid.shape[0]-1, 0, -1):
-            if y == grid.shape[0]-1 or grid[y,x] > bottom_max:
-                bottom_max = grid[y,x]
-                visible[y,x] = 1
-                
+            if x == 0 or grid[y, x] > left_max:
+                left_max = grid[y, x]
+                output_matrix[y, x] = 1
 
+                
+def count_edge_visible(grid):
+    visible = grid * 0
+    left = count_visible_left_edge(grid, visible)
+    right = count_visible_left_edge(np.flip(grid, 1), np.flip(visible, 1))
+    top = count_visible_left_edge(np.flip(grid.T, 1), np.flip(visible.T, 1))
+    bottom = count_visible_left_edge(grid.T, visible.T)
     return visible
 
 
 def scenic_score_right(grid, source_x, source_y):
-    
     for x in range(source_x+1, grid.shape[1]):  # Right Score
         if grid[source_y, x] >= grid[source_y, source_x] or x == grid.shape[1]-1:
             return x - source_x          
@@ -49,7 +32,6 @@ def scenic_score_right(grid, source_x, source_y):
     
  
 def assess_scenic(grid):
-    
     scenic_scores = grid*0
     
     for y in range(grid.shape[0]):
@@ -58,8 +40,7 @@ def assess_scenic(grid):
             left = scenic_score_right(np.flip(grid, 1), grid.shape[1]-x-1, y)
             top = scenic_score_right(np.flip(grid.T, 1), grid.shape[0]-y-1, x)
             bottom = scenic_score_right(grid.T, y, x)
-            scenic_scores[y,x] = right * left * top * bottom
-            
+            scenic_scores[y, x] = right * left * top * bottom
     return scenic_scores
  
 visible = count_edge_visible(grid) 
